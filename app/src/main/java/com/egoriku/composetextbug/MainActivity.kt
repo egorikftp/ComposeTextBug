37728ba189cca3e4a49df235fd386b94b7d6b224
yahor_urbanovich <egorikftp@gmail.com>
2022-01-13 12:18:11 +0300

    Reproduce issue with swipe

--- app/src/main/java/com/egoriku/composetextbug/MainActivity.kt
+++ app/src/main/java/com/egoriku/composetextbug/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.egoriku.composetextbug
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.MotionLayout
 import androidx.constraintlayout.compose.MotionLayoutDebugFlags
@@ -35,56 +34,78 @@ class MainActivity : AppCompatActivity() {
             MaterialTheme {
                 Column(modifier = Modifier.fillMaxSize()) {
                     var text by remember { mutableStateOf(TEXT) }
-                    var animateToEnd by remember { mutableStateOf(false) }
 
                     Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                        Button(onClick = { animateToEnd = !animateToEnd }) {
-                            Text(text = "Run")
-                        }
                         Button(onClick = { text = TEXT + System.currentTimeMillis() }) {
                             Text(text = "Change text")
                         }
                     }
 
-                    MotionBug(
-                        text = text,
-                        animateToEnd = animateToEnd
-                    )
+                    MotionSwipeBug(text = text)
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-private fun MotionBug(text: String, animateToEnd: Boolean) {
-    Log.d("bugg", text)
+private fun MotionSwipeBug(text: String) {
 
-    val progress by animateFloatAsState(
-        targetValue = if (animateToEnd) 1f else 0f,
-        animationSpec = tween(2000)
-    )
+    val componentHeight by remember { mutableStateOf(2000f) }
+    val swipeableState = rememberSwipeableState("Bottom")
+    val anchors = mapOf(0f to "Bottom", componentHeight to "Top")
+
+    val progress = (swipeableState.offset.value / componentHeight)
+
     MotionLayout(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(Color.White)
+            .swipeable(
+                state = swipeableState,
+                anchors = anchors,
+                reverseDirection = true,
+                orientation = Orientation.Vertical
+            ),
         motionScene = MotionScene(
             """{
                     ConstraintSets: {
                       start: {
-                        title: {
+                        icon: {
                           width: 'wrap',
-                          height: 'wrap',
                           start: ['parent', 'start', 16],
-                          bottom: ['parent', 'bottom', 16]
+                          bottom: ['parent', 'bottom', 16],
+                          height: 'wrap'
+                        },
+                        title: {
+                          width: 'spread',
+                          height: 'wrap',
+                          start: ['icon', 'end', 16],
+                          end: ['parent', 'end', 16],
+                          bottom: ['icon', 'bottom'],
+                          top: ['icon', 'top'],
+                          custom: {
+                            textSize: 10
+                          }
                         }
                       },
                       end: {
-                        title: {
+                        icon: {
                           width: 'wrap',
+                          start: ['parent', 'start', 16],
+                          bottom: ['parent', 'bottom', 16],
+                          height: 'wrap'
+                        },
+                        title: {
+                          width: 'spread',
                           height: 'wrap',
+                          bottom: ['icon', 'top', 16],
+                          start: ['parent', 'start', 16],
                           end: ['parent', 'end', 16],
-                          top: ['parent', 'top', 16]
+                          custom: {
+                            textSize: 25
+                          }
                         }
                       }
                     }
@@ -93,9 +114,17 @@ private fun MotionBug(text: String, animateToEnd: Boolean) {
         debug = EnumSet.of(MotionLayoutDebugFlags.SHOW_ALL),
         progress = progress
     ) {
+        Image(
+            modifier = Modifier.layoutId("icon"),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = ""
+        )
         Text(
             modifier = Modifier.layoutId("title"),
-            text = text
+            text = text,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontSize = motionFontSize("title", "textSize")
         )
     }
 }
